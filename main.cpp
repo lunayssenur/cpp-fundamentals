652513bbdf8cd1cb911bc7b996f828407c512290
Ayşenur Özkan <lunayssenur@gmail.com>
2026-02-22 16:07:04 +0300

    ilk ders kodum

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>   // Girdi ve çıktı işlemleri için gerekli kütüphane.
+using namespace std;  // Tekrar tekrar std yazmamak için kullanırız.
+int main() 
+{   // 1.DERS: Ekrana Yazdırma ve Alt Satıra Geçme
+    cout << "Merhaba dünya" << endl; // 'endl' imleci alt satıra taşır.
+    cout << "Bu bir programlama dersidir \n"; // '\n' de alt satıra geçmek için kullanır.
+    return 0 ;
+}
