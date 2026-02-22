94a70fe1a44bbdbbae504bb81d1171a365a00208
Ayşenur Özkan <lunayssenur@gmail.com>
2026-02-22 19:06:27 +0300

    Ikinci ders dosyasi olusturuldu

---
+++ ders2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>   
+using namespace std; 
+int main() 
+{   // 2.DERS: Matematiksel İşlemler
+    // Sayıları tırnak içine almazsan bilgisayar onları sayı olarak görür ve işlem yapar.
+    cout << "5 ile 8'in toplami sudur:" ;
+    cout << (5+8) << endl;
+    cout << "8'den 5 cikarsa sonuc:" << (8-5) << endl;
+    
+}
