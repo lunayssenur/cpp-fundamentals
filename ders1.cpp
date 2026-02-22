c7a533c745718df6d03150954111e96017db30e6
Ayşenur Özkan <lunayssenur@gmail.com>
2026-02-22 19:30:40 +0300

    Ders 1 ve Ders 2 icerikleri ayristirildi

---
+++ ders1.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Girdi ve çıktı işlemleri içingerekli kütüphane.
+using namespace std; // Her seferinde std:: yazmamak için kullanılır.
+int main()
+{ // 1.DERS: Ekrana Yazdırma ve Alt Satıra Geçme
+    cout << "Merhaba Dünya!" << endl; // 'endl' imleci alt satıra taşır.
+    cout << "Bu bir programlama dersidir.\n"; // '\n' de alt satıra geçmek için kullanılır.
+
+}
